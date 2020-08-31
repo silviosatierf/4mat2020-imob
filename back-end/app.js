4fdde09b7849609ed64ad7478f37ce4c2e765e04
fcintra4 <github4@faustocintra.com.br>
2020-08-31 14:07:28 +0000

    (31/08) Término da configuração de conexão a banco de dados

--- back-end/app.js
+++ back-end/app.js
@@ -7,7 +7,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const db = require('./config/database')
-// db('mongodb+srv://fausto:<password>@cluster0.31jwc.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority)
+const dbUser = process.env.DB_USER
+const dbPass = process.env.DB_PASS
+const dbName = process.env.DB_NAME
+db(`mongodb+srv://${dbUser}:${dbPass}@cluster0.31jwc.gcp.mongodb.net/${dbName}?retryWrites=true&w=majority`)
 
 var app = express();
 
