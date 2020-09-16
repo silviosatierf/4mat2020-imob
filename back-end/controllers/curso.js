14b3b1665bbb87a00b6ed217434e4e471a30f787
fcintra4 <github4@faustocintra.com.br>
2020-09-16 14:05:42 +0000

    (16/09) Término do primeiro CRUD

--- back-end/controllers/curso.js
+++ back-end/controllers/curso.js
@@ -39,7 +39,7 @@ controller.novo = async (req, res) => {
         // HTTP 201: Created
         res.status(201).end()
     }
-    catch(erro) {
+    catch (erro) {
         console.error(erro)
         // HTTP 500: Internal Server Error
         res.status(500).send(erro)
@@ -53,7 +53,7 @@ controller.listar = async (req, res) => {
         let dados = await Curso.find()
         res.send(dados) // Vai com status 200: OK
     }
-    catch(erro) {
+    catch (erro) {
         console.error(erro)
         res.status(500).send(erro)
     }
@@ -65,9 +65,46 @@ controller.obterUm = async (req, res) => {
     let obj = await Curso.findById(id)
 
     // Se o objeto vier preenchido (achou), então o retornamos
-    if(obj) res.send(obj)
+    if (obj) res.send(obj)
     // Senão (objeto vazio), enviamos o status HTTP 404: Not found
-    else res.status(404).end() 
+    else res.status(404).end()
+}
+
+// Método atualizar(), implementando a operação UPDATE
+controller.atualizar = async (req, res) => {
+    try {
+        // Isolar o _id do objeto para fins de busca
+        const id = req.body._id
+        // Busca o objeto pelo id e, encontrando-o, substitui o conteúdo por req.body
+        let obj = await Curso.findByIdAndUpdate(id, req.body)
+
+        // Se encontrou e substituiu, retornamos HTTP 204: No content
+        if (obj) res.status(204).end()
+        // Caso contrário, retorna HTTP 404: Not found
+        else res.status(404).end()
+    }
+    catch (erro) {
+        console.error(erro)
+        res.status(500).end()
+    }
+}
+
+// Método excluir(), implementando a operação DELETE
+controller.excluir = async (req, res) => {
+    try {
+        // Isolando o id para exclusão
+        const id = req.body._id
+        let obj = await Curso.findByIdAndDelete(id)
+
+        // Encontrou e excluiu
+        if(obj) res.status(204).end()
+        // Objeto não foi encontrado para exclusão
+        else res.status(404).end()
+    }
+    catch(erro) {
+        console.error(erro)
+        res.status(500).send(erro)
+    }
 }
 
 module.exports = controller
--- back-end/routes/curso.js
+++ back-end/routes/curso.js
@@ -4,6 +4,9 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/', controller.novo)       // Create
-router.get('/', controller.listar)      // Retrieve
+router.get('/', controller.listar)      // Retrieve (all)
+router.get('/:id', controller.obterUm)  // Retrieve (one)
+router.put('/', controller.atualizar)   // Update
+router.delete('/', controller.excluir)  // Delete
 
 module.exports = router
