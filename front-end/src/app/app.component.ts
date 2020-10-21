caecc4b85e7ae24cf9423d6c0773989d77bf528a
silviosatierf <silviosatierf@gmail.com>
2020-10-21 14:09:28 +0000

    (21/10) Criaçao do primeiro Angular

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+    <app-main-toolbar [appName]="title"></app-main-toolbar>
     <!--The content below is only a placeholder and can be replaced.-->
     <div style="text-align:center" class="content">
       <h1>
@@ -28,5 +29,5 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
-  title = 'front-end';
+  title = 'Escola Agora Vai';
 }
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { MainToolbarComponent } from './ui/main-toolbar/main-toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainToolbarComponent
   ],
   imports: [
     BrowserModule,
