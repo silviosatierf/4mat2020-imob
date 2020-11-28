3394c6314b46299212932844d26684319a86aeb8
silviosatierf <silviosatierf@gmail.com>
2020-11-28 23:53:14 +0000

    (25/11) Início de formulário com relacionamentos

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CursoListComponent } from './curso/curso-list/curso-list.component';
 import { CursoFormComponent } from './curso/curso-form/curso-form.component';
+import { TurmaFormComponent } from './turma/turma-form/turma-form.component';
 
 const routes: Routes = [
     // Rotas no Angular NUNCA  começam com /
-    { path: 'curso', component: CursoListComponent}, 
+    {path: 'curso', component: CursoListComponent}, 
     {path: 'curso/novo', component: CursoFormComponent},
     {path: 'curso/:id', component: CursoFormComponent},
-    {path: 'turma', component: TurmaListComponent}
-];
+
+    {path: 'turma', component: TurmaListComponent},
+    {path: 'turma/novo', component: TurmaFormComponent},
+    {path: 'turma/:id', component: TurmaFormComponent},
+    ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+import { MatMomentDateModule, MAT_MOMENT_DATE_FORMATS, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+import { MAT_DATE_LOCALE, MAT_DATE_FORMATS } from '@angular/material/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +17,8 @@ import { CursoListComponent } from './curso/curso-list/curso-list.component';
 import { CursoFormComponent } from './curso/curso-form/curso-form.component';
 import {​​ FormsModule }​​ from '@angular/forms';
 import { TurmaListComponent } from './turma/turma-list/turma-list.component';
+import { TurmaFormComponent } from './turma/turma-form/turma-form.component';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +28,8 @@ import { TurmaListComponent } from './turma/turma-list/turma-list.component';
     MainFooterComponent,
     CursoListComponent,
     CursoFormComponent,
-    TurmaListComponent
+    TurmaListComponent,
+    TurmaFormComponent
   ],
   imports: [
     BrowserModule,
@@ -30,9 +37,17 @@ import { TurmaListComponent } from './turma/turma-list/turma-list.component';
     BrowserAnimationsModule,
     MaterialModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatMomentDateModule
   ],
-  providers: [],
+  providers: [
+    // No app.module.ts, dentro seção providers
+    /**** Datas em português no MatDatepicker  ****/
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+    { provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: {useUtc: true}}
+    /**********************************************/ 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
